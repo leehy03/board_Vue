f6465c854c05d58c559a0c67b7bbcc869c103b2a
hy <leehy030427@naver.com>
2024-04-29 18:44:04 +0900

    feat: 라우터 등록

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import BoardListView from "../views/BoardListView.vue";
+import BoardViewView from "@/views/BoardViewView.vue";
+import BoardWriteView from "@/views/BoardWriteView.vue";
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    name: "list",
+    component: BoardListView,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/view",
+    name: "view",
+    component: BoardViewView,
+  },
+  {
+    path: "/write",
+    name: "write",
+    component: BoardWriteView,
   },
 ];
 
