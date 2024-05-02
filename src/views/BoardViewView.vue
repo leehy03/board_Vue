fc13c9551c78fee1aee981379d8cd5ba0e42586e
hy <leehy030427@naver.com>
2024-05-02 18:36:30 +0900

    feat: 게시글 상세 페이지 추가

--- src/views/BoardViewView.vue
+++ src/views/BoardViewView.vue
@@ -1,9 +1,45 @@
 <template>
-  <div>a</div>
+  <div>
+    {{ board.boardNo }}
+    {{ board.title }}
+    {{ board.author }}
+    {{ board.createdDt }}
+    {{ board.views }}
+  </div>
 </template>
 <script>
+import { ref, onMounted } from "vue";
+import Axios from "axios";
+import { useRoute } from "vue-router";
 export default {
   name: "BoardViewView",
-  setup() {},
+  setup() {
+    const route = useRoute();
+    const board = ref(new Object());
+
+    onMounted(() => {
+      searchBoardView();
+    });
+
+    const searchBoardView = () => {
+      Axios.get("http://localhost:8070/boards", {
+        params: {
+          no: route.query.no,
+        },
+      })
+        .then(function (response) {
+          console.log(response.data);
+          board.value = response.data;
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    };
+
+    return {
+      searchBoardView,
+      board,
+    };
+  },
 };
 </script>
